a165bdfb6530a38ab978c8d9966c9c03caae1ea9
NoctisAtrae <noctisatrae@gmail.com>
2021-06-12 12:32:18 +0200

    Je continue de développer l'API.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const marked = require('marked');
+const Gun = require('gun');
 const TerminalRenderer = require('marked-terminal');
+
+const config = require('../config.json');
+
+const PORT = config.port;
+
+const gun = Gun();
 
 marked.setOptions({
     renderer: new TerminalRenderer()
@@ -10,6 +17,43 @@ console.log(marked('# Starting Gunpoint API !'))
 
 const app = express();
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
-    res.json({ msg: "Welcome to Gunpoint API !" })
+    res.status(200).send({ msg: "Welcome to Gunpoint API !" })
 })
+
+app.get('/get/:key', (req, res) => {
+    
+    const { key } = req.params;
+    
+    let toFetch = gun.get(key);
+
+    toFetch.on((data) => { res.status(200).send({ msg: `Gunpoint has fetched this: '${key}'`, content: data }) })
+
+});
+
+app.post('/put/:key', (req, res) => {
+
+    const { key } = req.params;
+    const dataToAdd = req.body;
+
+    if (!dataToAdd) {
+        console.log("No data !");
+        res.status(418).sendFile({ error: "There's no data in your request !" })
+    } else {
+        let toFetch = gun.get(key);
+        let dataFetched;
+    
+        toFetch.put(dataToAdd)
+        toFetch.on((data) => { 
+            res.status(200).json({success: "YEAH !",dataAdded:dataToAdd, content: data }) 
+        })
+    }
+})
+
+app.post('/debug/:param', (req, res) => {
+    res.json(req.body);
+})
+
+app.listen(PORT, () => { console.log(marked('**Gunpoint is running at http://localhost:' + PORT + '**')) })
